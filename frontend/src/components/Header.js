31a2dfeabdb0ee415855f08d470c63cfe0d0ffff
Harsh-Wardhan-Dohaiya <hwardhand01@gmail.com>
2022-04-14 23:47:42 +0530

    added lists option

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                                <Link className="nav-link active" to="/">Home</Link>
+                                <Link className="nav-link " to="/">Home</Link>
                                 {/* <a className="nav-link active" aria-current="page" href="home.html">Home</a> */}
                             </li>
                             <li className="nav-item">
